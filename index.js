0e1b3eee6954b1c3cc305cf42641a1e873a5b3be
yassineAlouini <yassinealouini@outlook.com>
2015-07-02 17:32:04 +0100

    Some more tweaking

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var browserWindow = require('browser-window')
 app.on('ready', function () {
   var mainWindow = new browserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: 'myApp_icon.png'
   });
   mainWindow.loadUrl('file://' + __dirname + '/index.html')
 });
